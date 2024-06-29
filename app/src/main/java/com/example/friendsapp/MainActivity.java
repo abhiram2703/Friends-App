e05a34820f810fdb25a7b64079bec6eac381bb90
abhiram2703 <abhirambiju2703@gmail.com>
2024-06-29 15:30:10 +0530

    Updated with networking

--- app/src/main/java/com/example/friendsapp/MainActivity.java
+++ app/src/main/java/com/example/friendsapp/MainActivity.java
@@ -1,24 +1,84 @@
 package com.example.friendsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    EditText et1,et2,et3,et4;
+    AppCompatButton bt1;
+    String apiurl="https://friendsapi-re5a.onrender.com/adddata";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        et1=(EditText) findViewById(R.id.nameet);
+        et2=(EditText) findViewById(R.id.fnameet);
+        et3=(EditText) findViewById(R.id.fnnameet);
+        et4=(EditText) findViewById(R.id.descet);
+        bt1=(AppCompatButton) findViewById(R.id.subbtn);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getName=et1.getText().toString();
+                String getFname=et2.getText().toString();
+                String getFnname=et3.getText().toString();
+                String getDesc=et4.getText().toString();
+
+                JSONObject friend=new JSONObject();
+                try {
+                    friend.put("name",getName);
+                    friend.put("friendName",getFname);
+                    friend.put("friendNickName",getFnname);
+                    friend.put("DescribeYourFriend",getDesc);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiurl,
+                        friend,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "SUCCESSFUL", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(),"ERROR",Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                );
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
+
+
+
+            }
         });
+
     }
 }
